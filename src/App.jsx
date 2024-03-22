5ea978045b734770cb45d14cfcb9c2b53b334805
D.B.S.S Dassanayake <bhanukadassanayake@gmail.com>
2024-03-22 12:59:09 +0530

    setup for game init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import GladGame from "./GladiatorGame/GladGame";
+import NanthuMCQ from "./components/NanthuMCQ";
 
 export default function App() {
-  return <GladGame />;
+  return (
+    <>
+      <div className=" h-[65vh] bg-red-400"></div>
+      <NanthuMCQ />
+    </>
+  );
 }
---
+++ src/components/NanthuMCQ.jsx
@@ -0,0 +1,3 @@
+export default function NanthuMCQ() {
+  return <div>1x2=2 coponents</div>;
+}
