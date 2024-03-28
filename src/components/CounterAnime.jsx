245d58a43fa5740f9967a746c812a4298621c458
D.B.S.S Dassanayake <bhanukadassanayake@gmail.com>
2024-03-28 20:18:26 +0530

    Homepage Building 5

--- src/components/CounterAnime.jsx
+++ src/components/CounterAnime.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import AnimatedNumber from "react-animated-numbers";
+import { BrowserView, MobileView } from "react-device-detect";
 
 export default function CounterAnime({
   children = 0,
@@ -10,13 +11,18 @@ export default function CounterAnime({
   let end = parseFloat(children, 10);
 
   return (
-    <AnimatedNumber
-      transitions={(index) => ({
-        type: "spring",
-        duration: index + 0.1,
-      })}
-      animateToNumber={end}
-      className={className}
-    />
+    <>
+      <BrowserView>
+        <AnimatedNumber
+          transitions={(index) => ({
+            type: "spring",
+            duration: index + 0.1,
+          })}
+          animateToNumber={end}
+          className={className}
+        />
+      </BrowserView>
+      <MobileView>{end}</MobileView>
+    </>
   );
 }
